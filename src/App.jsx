fd221ff71b3822074fb7dac534593e1d2b75a967
Patrickx7 <p.jorgemoura@gmail.com>
2026-08-27 18:38:15 -0300

    Atualização do projeto

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react"
+
+function App() {
+
+  const [titulo, setTitulo] = useState('')
+  const [tarefas, setTarefas] = useState([])
+  const [erro, setErro] = useState ('')
+
+  function adicionarTarefa() {
+    if (titulo.trim() === '') {
+      setErro('Digite uma tarefa!')
+      return
+    }
+
+    const novaTarefa = { id: Date.now(), titulo: titulo.trim(), criadaEm: new Date().toLocaleString('pt-BR') }
+    setTarefas([...tarefas, novaTarefa])
+    setTitulo('')
+
+  }
+
+  function excluirTarefa(id) {
+    if (!confirm('Tem certeza?')) {
+      return
+    }
+
+    setTarefas(tarefas.filter(tarefa => tarefa.id !== id))
+  }
+
+  return (
+    <div>
+      <h1>Lista de tarefas</h1>
+      <input maxLength={100} value={titulo} onChange={(e) => setTitulo(e.target.value)} type="text" placeholder="Nova tarefa..." />
+      <button onClick={adicionarTarefa}>Adicionar</button>
+      <p>{titulo.length}/100</p>
+      {tarefas.length === 0 && <p>Nenhuma tarefa</p>}
+
+
+      <ul>
+        {tarefas.map(tarefa => (
+          <li key={tarefa.id}>
+            <button onClick={() => excluirTarefa(tarefa.id)}>X</button>
+            {tarefa.titulo}
+            {tarefa.criadaEm}
+          </li>
+
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App
+
+
